37794222740aed1db9bf7577e4690fa2546b09e5
Sahil <sahilahmed466@gmail.com>
2024-05-13 15:22:03 +0530

    Setup Created

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,9 @@ module.exports = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        dmsans: ["DM Sans", "sans-serif"],
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
