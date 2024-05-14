a19435d2cf548366f4c280cd3837952530a2dba8
Sahil <sahilahmed466@gmail.com>
2024-05-14 16:42:19 +0530

    Integrate Chart.js

--- app/components/graph/Graph.jsx
+++ app/components/graph/Graph.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Graph() {
   return (
-    <div className='min-w-[312px] h-[213px] w-full rounded-[12px] bg-[#282828]'>Graph</div>
+    <div className='min-w-[312px] h-[213px] z-20 w-full rounded-[12px] bg-[#282828] flex items-center justify-center'>
+      {/** graph container */}
+      <div className='w-[284px] h-[169px] bg-gray-700'>graph</div>
+    </div>
   )
 }
 
